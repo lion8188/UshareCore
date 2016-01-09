7510db7ace7268a32c62f4a31e053609a4f5562c
lion_mac_air <lion8188@googlemail.com>
2016-01-10 00:08:57 +0100

    create interest table and test data

---
+++ db/migration/create-test_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO interest (id, latitude, longtitude, address, type_code, name, description, publisher)
+VALUES ('int00000001','12.12345','34.54321','add00000001','1','KFC','Best KFC ever', 'user00000001');
+
--- db/schema/create-tables.sql
+++ db/schema/create-tables.sql
@@ -1,6 +1,28 @@
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS user (
 	id VARCHAR(20) NOT NULL,
 	name VARCHAR(40) NOT NULL,
 	CONSTRAINT user1_p PRIMARY KEY (id));
 
+CREATE TABLE IF NOT EXISTS interest (
+    id VARCHAR(20) NOT NULL,
+    latitude FLOAT(10,6),
+    longtitude FLOAT(10,6),
+    address VARCHAR(20),
+    type_code TINYINT(2),
+    name VARCHAR(255),
+    description TEXT,
+    publisher VARCHAR(20),
+    CONSTRAINT interest1_p PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS address (
+    id VARCHAR(20) NOT NULL,
+    CONSTRAINT address1_p PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS interest_type(
+    id VARCHAR(20) NOT NULL,
+    name VARCHAR(255),
+    CONSTRAINT interest_type1_p PRIMARY KEY (id)
+);
 
